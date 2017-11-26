8d2556dbcc4d277e7dae6b6965533bba4d2daf5c
NuclearManD <dylanbrophy@gmail.com>
2017-11-26 08:09:27 -0800

    no java!

--- uJ/BUILDENV/RT/real/java/lang/Runnable.java
+++
@@ -1,6 +0,0 @@
-package java.lang;
-
-public interface Runnable{
-
-	public void run();
-}
--- uJ/uc_loader.h
+++
@@ -1,9 +0,0 @@
-#ifndef _UC_LOADER_H_
-#define _UC_LOADER_H_
-
-
-UInt32 uc_loader();	//return actual area size
-
-
-
-#endif
