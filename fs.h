c1c7f3cb23ed2073f1c7465e155f340a9f8532e1
NuclearManD <dylanbrophy@gmail.com>
2018-04-22 10:36:13 -0700

    q

--- fs.h
+++ fs.h
@@ -2,6 +2,7 @@
 #define SDFS_H
 #include <SPI.h>
 #include <SD.h>
+//#include <EEPROM.h>
 //#include "ROM_fs.h"
 
 bool sd_mounted=false;
@@ -9,11 +10,81 @@ bool mount(){
   if(sd_mounted)
     return true;
   pinMode(53, OUTPUT);
-  sd_mounted=SD.begin(53);
+  pinMode(5, OUTPUT);
+  sd_mounted=SD.begin(5);
   return sd_mounted;
 }
-char* curdir;
+char curdir[256];
 void init_fs(){
-  curdir="/";
+  curdir[0]='/';
+  curdir[1]=0;
+}
+char fs_buffer[256];
+char* fs_resolve(char* loc){
+  char tmp[32];
+  int index=0;
+  int copied=0;
+  int last_index=0;
+  if(loc[0]!='/'){
+    for(int i=0;i<=strlen(curdir);i++){
+      if(curdir[i]=='/'||curdir[i]==0){
+        if(!strcmp(tmp,".")){
+          ;
+        }else if(!strcmp(tmp,"..")){
+          index=last_index;
+        }else{
+          last_index=index;
+          for(int j=0;j<copied;j++){
+            fs_buffer[index]=tmp[j];
+            index++;
+          }
+          if(fs_buffer[index-1]!='/'){
+            fs_buffer[index]='/';
+            index++;
+          }
+        }
+        copied=0;
+      }else{
+        tmp[copied]=curdir[i];
+        copied++;
+      }
+    }
+    copied=0;
+  }
+  for(int i=0;i<=strlen(loc);i++){
+    if(loc[i]=='/'||loc[i]==0){
+      if(!strcmp(tmp,".")){
+        ;
+      }else if(!strcmp(tmp,"..")){
+        index=last_index;
+      }else{
+        last_index=index;
+        for(int j=0;j<copied;j++){
+          fs_buffer[index]=tmp[j];
+          index++;
+        }
+        if(fs_buffer[index-1]!='/'){
+          fs_buffer[index]='/';
+          index++;
+        }
+      }
+      copied=0;
+    }else{
+      tmp[copied]=loc[i];
+      copied++;
+    }
+  }
+  if(loc[strlen(loc)-1]=='/'){
+    if(fs_buffer[index-1]!='/')
+      fs_buffer[index]='/';
+    fs_buffer[index+1]=0;
+  }else if(fs_buffer[index-1]=='/'){
+    fs_buffer[index-1]=0;
+  }else
+    fs_buffer[index]=0;
+  return fs_buffer;
+}
+bool mkdir(char* name){
+  return SD.mkdir((const char *)fs_resolve(name));
 }
 #endif
