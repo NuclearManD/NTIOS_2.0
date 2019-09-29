68f08d8757b0bbe02e327d90e964296e2d5f297a
NuclearManD <dylanbrophy@gmail.com>
2019-09-29 01:46:02 -0700

    Cleaning - still on repo and in old commits - should be fine

--- Arduino_desktop_firmware_1711_thusfar/basic.h
+++
@@ -1,8 +0,0 @@
-#ifndef BASIC_CPP
-#define BASIC_CPP
-#include <avr/pgmspace.h>
-#include <Arduino.h>
-int compile(char* src, char* dst);
-int executeCompiledCode(byte* pgm, int length);
-void setup_basic(void (println_f)(char*));
-#endif
