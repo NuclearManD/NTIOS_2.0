bce039e49766d86ea28c836105ffac2f7f7564d4
NuclearManD <dylanbrophy@gmail.com>
2019-10-08 20:07:33 -0700

    added termctl command to use/test dynamic driver creation and destruction.  WIP

---
+++ programs/prg_termctl/main.c
@@ -0,0 +1,103 @@
+#include "../../include/driver_api.h"
+#include "../../include/kernel_functions.h" // new and delete on AVR
+
+int ls_termctl_instance = 0;
+
+class TermCtl: public Terminal{
+public:
+	TermCtl(Terminal* in, Terminal* out){
+		input = in;
+		output = out;
+		snprintf(name, 7, "tctl%i", ls_termctl_instance);
+		ls_termctl_instance++;
+	}
+	void stdo(char* d){
+		output->stdo(d);
+	}
+	void stde(char* d){
+		output->stde(d);
+	}
+	char read(){
+		return input->read();
+	}
+	bool available(){
+		return input->available();
+	}
+	char* get_path(){
+		return name;
+	}
+protected:
+	Terminal* input;
+	Terminal* output;
+	char name[7];
+};
+
+int termctl_main(char** argv, int argc){
+	if(argc<2){
+		stdo("usage: termctl -m [in] [out]\n       termctl -l\n       termctl -u [dev]\n       termctl -r [dev]");
+		return 0;
+	}
+	char* cmd = argv[1];
+	if(!strcmp(cmd, "-l")){
+		for(int i=0;i<num_drivers();i++){
+			Driver* dev = get_driver(i);
+			if(dev->get_type()==DRIVER_TYPE_TERM){
+				stdo(dev->get_path());
+				stdo("\n");
+			}
+		}
+	}else if(!strcmp(cmd, "-m")){
+		if(argc==4){
+			Terminal* input = 0;
+			Terminal* output = 0;
+			bool failure = false;
+			
+			for(int i=0;i<num_drivers();i++){
+				Driver* dev = get_driver(i);
+				if(dev->get_type()==DRIVER_TYPE_TERM){
+					if(!strcmp(argv[2], dev->get_path())){
+						input = (Terminal*)dev;
+					}
+					if(!strcmp(argv[3], dev->get_path())){
+						output = (Terminal*)dev;
+					}
+				}
+			}
+			
+			if(input==0){
+				failure = true;
+				stde(argv[2]);
+				stde(" is not a valid terminal.");
+			}
+			if(output==0){
+				failure = true;
+				stde(argv[3]);
+				stde(" is not a valid terminal.");
+			}
+			if(!failure){
+				TermCtl* new_term = new TermCtl(input, output);
+				int result = add_driver(new_term);
+				if(result<0){
+					stde("Unknown error!");
+					return result-100;
+				}else{
+					stdo(new_term->get_path());
+				}
+			}else return -1;
+		}else{
+			stde("termctl -m [in] [out]");
+			return -1;
+		}
+	}
+	return 0;
+}
+
+void termctl_init(){
+	#ifdef MAN_INSTALLED
+/*	man_add_topic("dev", "\t\tdev - a tool for driver management\n\
+Usage: dev -l    : lists driver paths\n\
+       dev [driver path]  : info on driver\n\
+\n\
+\tThere is not much here yet. Come again soon :)");*/
+	#endif
+}
